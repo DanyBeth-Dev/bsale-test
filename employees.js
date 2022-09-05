00f8abdb1c4a98f18de5c27f54bf7629f4977ba8
Daniela Hernández <105611396+DanyBeth-Dev@users.noreply.github.com>
2022-09-05 00:32:08 -0300

    ok

---
+++ employees.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+
+const { pool } = require('./database.js')
+
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+router.get('/bebida-energetica', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/pisco', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/ron', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/bebida', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/snack', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/cerveza', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/vodka', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/search/shows?=:query', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+router.get('/data/', (req, res) => {
+  pool.query('SELECT * FROM product', (err, rows, fields) => {
+    if(!err) {
+      //console.log(rows)
+      //results=JSON.parse(JSON.stringify(results))
+      res.json(rows)
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+router.get('/data/:producto', async (req, res) => {
+  const { producto } = req.params
+  const productoFormateado = producto.replace(/-/g, " ")
+  pool.query(`SELECT * from product WHERE category IN (SELECT id FROM category WHERE name = "${productoFormateado}")`, (err, rows, fields) => {
+    if (!err) {
+      let filas = JSON.parse(JSON.stringify(rows))
+      //console.log('filas: ', filas)
+      res.json(rows)
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+router.get('/search', (req, res) => {
+  const { producto } = req.query
+  productoFormateado = producto
+  //console.log(productoFormateado)
+
+  pool.query(`SELECT * from product WHERE category IN (SELECT id FROM category WHERE name = "${productoFormateado}")`, (err, rows, fields) => {
+    if (!err) {
+      let filas = JSON.parse(JSON.stringify(rows))
+      //console.log('filas: ', filas)
+      res.json(rows)
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+module.exports = router
